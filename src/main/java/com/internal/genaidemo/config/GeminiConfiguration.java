4ce42e114c557cfdfee30ce8212f666de16b7fbf
muskan2597 <yadavmuskaan099@gmail.com>
2024-10-01 20:23:21 +0530

    setting env variables

--- src/main/java/com/internal/genaidemo/config/GeminiConfiguration.java
+++ src/main/java/com/internal/genaidemo/config/GeminiConfiguration.java
@@ -2,15 +2,22 @@ package com.internal.genaidemo.config;
 
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 public class GeminiConfiguration {
 
+    @Value("${google.project-id}")
+    private String PROJECT_ID;
+
+    @Value("${vertex-ai.location}")
+    private String LOCATION;    // e.g., "us-central1"
+
     @Bean
     public VertexAI vertexAI(){
-        return new VertexAI("sep-rjha-08dec-cts","us-central1");
+        return new VertexAI(PROJECT_ID,LOCATION);
     }
 
     public GenerativeModel generativeModel(VertexAI vertexAI){
