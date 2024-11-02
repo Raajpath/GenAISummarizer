63b424afbedf40ce5a21f39f8acda111a2da8ffa
Jha <2048748@cognizant.com>
2024-11-02 20:50:59 +0530

    Handled blocked content scenario & improved prompt

--- src/main/java/com/internal/genaidemo/config/GeminiConfiguration.java
+++ src/main/java/com/internal/genaidemo/config/GeminiConfiguration.java
@@ -1,10 +1,17 @@
 package com.internal.genaidemo.config;
 
 import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.HarmCategory;
+import com.google.cloud.vertexai.api.SafetySetting;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.cloud.vertexai.api.HarmCategory.*;
 
 @Configuration(proxyBeanMethods = false)
 public class GeminiConfiguration {
@@ -25,7 +32,12 @@ public class GeminiConfiguration {
 
     @Bean
     public GenerativeModel geminiProVisionGenerativeModel(VertexAI vertexAI) {
-        return new GenerativeModel(MODEL_NAME, vertexAI);
+        return new GenerativeModel(MODEL_NAME, vertexAI).
+                withSafetySettings(List.of(
+                        SafetySetting.newBuilder().setCategory(HarmCategory.HARM_CATEGORY_HATE_SPEECH).setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_NONE).build(),
+                        SafetySetting.newBuilder().setCategory(HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT).setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_NONE).build(),
+                        SafetySetting.newBuilder().setCategory(HarmCategory.HARM_CATEGORY_HARASSMENT).setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_NONE).build(),
+                        SafetySetting.newBuilder().setCategory(HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT).setThreshold(SafetySetting.HarmBlockThreshold.BLOCK_NONE).build()));
     }
 
 
